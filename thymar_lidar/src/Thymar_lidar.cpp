b491eff1582220e64790ef907458d2cdad2fcacf
usi <user.name>
2020-05-11 20:17:29 +0200

    publish a marker at the target position when it is found

--- thymar_lidar/src/Thymar_lidar.cpp
+++ thymar_lidar/src/Thymar_lidar.cpp
@@ -5,6 +5,7 @@
 #include "pcl_conversions/pcl_conversions.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "nav_msgs/Odometry.h"
+#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Pose.h>
 #include <tf/tf.h>
 #include <string>
@@ -23,6 +24,7 @@ private:
     ros::Subscriber odometry_subscriber;
     ros::Publisher obstacles_publisher;
     ros::Publisher terrain_publisher;
+    ros::Publisher target_marker_publisher;
 
 
 
@@ -40,6 +42,7 @@ public:
 	~ThymarLidar(){delete mapper;};
 	void processLidarMeasurement(const pcl::PCLPointCloud2ConstPtr& cloud_msg);
 	void parseOdometry(const nav_msgs::Odometry::ConstPtr& msg);
+	void publishTargetMarker();
 	void run();
        
 };
@@ -65,6 +68,8 @@ ThymarLidar::ThymarLidar(int argc, char** argv, float hz, int grid_width, int gr
 		this->grid.info.height = (int) std::round(grid_height/grid_resolution);
 
 		this->mapper= new PointCloudMapper((int) std::round(grid_width/grid_resolution), (int) std::round(grid_height/grid_resolution), grid_resolution);
+
+		this->target_marker_publisher = this->nh.advertise<visualization_msgs::Marker>("/" +this->name +"/target_marker", 1);
 
 		std::cout << "Lidar PointCloud Processor node initialised for robot " << this->name << std::endl;
 	
@@ -125,6 +130,8 @@ void ThymarLidar::run(){
         		std::cout << "Model found in (" << this->target_model.x << ","  << this->target_model.y << ") " 
 				<< "with radius: "<< this->target_model.radius 
 				<< std::endl;
+				this->publishTargetMarker();
+
         	}
 		}
 
@@ -133,6 +140,41 @@ void ThymarLidar::run(){
 	}
 }
 
+void ThymarLidar::publishTargetMarker(){
+	visualization_msgs::Marker marker;
+	
+	marker.header.frame_id = "/thymar/base_link";
+	//marker.header.stamp = ros::Time::now();
+
+	marker.ns = "basic_shapes";
+	marker.id = 4;
+
+	marker.type = visualization_msgs::Marker::SPHERE;
+	marker.action = visualization_msgs::Marker::ADD;
+
+	marker.pose.position.x = this->target_model.x;
+	marker.pose.position.y = this->target_model.y;
+	marker.pose.position.z = this->target_model.radius;
+	marker.pose.orientation.x = 0.0;
+	marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0;
+	marker.pose.orientation.w = 1.0;
+
+	// Set the scale of the marker -- 1x1x1 here means 1m on a side
+	marker.scale.x = this->target_model.radius*2.0;
+	marker.scale.y = this->target_model.radius*2.0;
+	marker.scale.z = this->target_model.radius*2.0;
+
+	// Set the color -- be sure to set alpha to something non-zero!
+	marker.color.r = 0.0f;
+	marker.color.g = 1.0f;
+	marker.color.b = 0.0f;
+	marker.color.a = 1.0;
+
+	marker.lifetime = ros::Duration(0.0);;
+
+	this->target_marker_publisher.publish(marker);
+}
 
 
 int main(int argc, char** argv)
